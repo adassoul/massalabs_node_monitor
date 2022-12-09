4aa7dfa382c296b336a37929815ddbad3bf0e7e8
adassoul <aymane.dassouli@gmail.com>
2022-12-09 22:25:09 +0100

    navbar done + darkmode logic working

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log(window.massa)
+
+// console.log(_1)
+
+// var checkbox = document.querySelector("input[name='checkbox']");
+var checkbox = document.querySelector(".checkbox")
+const container = document.querySelector('.container')
+console.log(checkbox)
+console.log(container)
+checkbox.addEventListener('change', ()=>{
+  container.classList.toggle('dark');
+})
+
+// const checkbox = document.getElementById('checkbox');
+
+// checkbox.addEventListener('change', ()=>{
+//   document.body.classList.toggle('dark');
+// })
