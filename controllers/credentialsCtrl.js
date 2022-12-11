788da74235692677df4451bea2990c99107d06f9
adassoul <aymane.dassouli@gmail.com>
2022-12-11 12:29:14 +0100

    login button for the 3 views

--- controllers/credentialsCtrl.js
+++ controllers/credentialsCtrl.js
@@ -1,4 +1,9 @@
 console.log("credentialsCtrl.js")
+/**
+ * input error handling
+ */
+const wls = window.localStorage;
+
 
 const handleCredentialsTextInput = (className, charNumb, startsWith) => {
   var input = document.querySelector(`.input-container.${className} input`)
@@ -16,6 +21,7 @@ const handleCredentialsTextInput = (className, charNumb, startsWith) => {
     if((input.value.length === charNumb)&&(input.value.substring(0,2) === startsWith)){
       result = input.value;
       error.classList.add("hidden")
+      wls.setItem(className, result)
     }
     else{
       result = 0;
@@ -26,9 +32,28 @@ const handleCredentialsTextInput = (className, charNumb, startsWith) => {
   })
 }
 
+
+/**
+ * handle connexion to client
+ */
+
+const handleClientCreation = () => {
+  console.log("client connexion")
+  console.log(wls.getItem("private-key"))
+  console.log(wls.getItem("public-key"))
+  console.log(wls.getItem("address"))
+  wls.clear()
+}
+
+
+/**
+ * main
+ */
 //private key: length 52 starts with S1
 handleCredentialsTextInput("private-key", 52, "S1")
 //public key: length 51 starts with P1
 handleCredentialsTextInput("public-key", 51, "P1")
 //address: length 51 starts with  A1
 handleCredentialsTextInput("address", 51, "A1")
+
+handleClientCreation()
