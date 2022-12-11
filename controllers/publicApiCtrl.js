62341cb4fb49e22d7b94a16640f8456d1d45b036
adassouli <a.dassouli@izicap.com>
2022-12-11 22:13:42 +0100

    connected credCtrl.js to puApiCtr.js

--- controllers/publicApiCtrl.js
+++ controllers/publicApiCtrl.js
@@ -1,2 +1,6 @@
+console.log(clientTest);
+import { clientTest} from "./credentialsCtrl.js";
 console.log("publicApiCtrl.js")
 
+let wls = window.localStorage;
+console.log(wls.getItem("massaClient"))
